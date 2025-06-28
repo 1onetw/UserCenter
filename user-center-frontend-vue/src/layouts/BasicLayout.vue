c924655c3f73562fca4e295b75ebd838ead6bc99
lunchangzhou <12278949+on-chang-zhou@user.noreply.gitee.com>
2025-06-29 07:44:09 +0800

    引入组件Antdesign，并创建通用布局BasicLayout

--- user-center-frontend-vue/src/layouts/BasicLayout.vue
+++ user-center-frontend-vue/src/layouts/BasicLayout.vue
@@ -1,10 +1,18 @@
+<template>
+  <div id="basicLayout">
+    <a-layout>
+      <a-layout-header :style="headerStyle">Header</a-layout-header>
+      <a-layout-content :style="contentStyle">Content</a-layout-content>
+      <a-layout-footer :style="footerStyle">Footer</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+
 <script setup lang="ts">
 
 </script>
 
-<template>
-  $END$
-</template>
 
 <style scoped>
 
